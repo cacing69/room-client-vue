3b4c97f381aa61ffbd559829ab83b1f856865a46
cacing69 <ibnuul@gmail.com>
2022-08-08 10:06:10 +0700

    up type

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,8 +10,9 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <van-button round type="primary" @click="count++">v5.2.2 count is {{ count }}</van-button>
+    <van-button round type="primary" @click="count++">count is {{ count }}</van-button>
   </div>
+  <small>v 1.0</small>
 </template>
 
 <style scoped>
