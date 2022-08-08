c1ed30bb1c7a79cf8816432fdc3d06b61bc737a0
cacing69 <ibnuul@gmail.com>
2022-08-08 10:13:40 +0700

    up type

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue'
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+const reload = () => window.location.reload(true)
 </script>
 
 <template>
@@ -13,6 +14,7 @@ const count = ref(0)
     <van-button round type="primary" @click="count++">count is {{ count }}</van-button>
   </div>
   <small>v 1.0</small>
+  <van-button round type="primary" @click="reload" size="mini">count is {{ count }}</van-button>
 </template>
 
 <style scoped>
